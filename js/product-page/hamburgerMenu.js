485c2438c30f674d7bd0ad822b0ccc33f149ab2b
Bohdan <banzai2003@gmail.com>
2021-07-03 17:27:10 +0300

    Formatted html, css and js for product page

--- js/product-page/hamburgerMenu.js
+++ js/product-page/hamburgerMenu.js
@@ -1,19 +1,19 @@
-let menu = document.querySelector(".menu");
-let ham = document.querySelectorAll(".ham");
-let menuIcon = document.querySelector(".menuIcon");
-let xIcon = document.querySelector(".xIcon");
+let menu = document.querySelector('.menu');
+let ham = document.querySelectorAll('.ham');
+let menuIcon = document.querySelector('.menuIcon');
+let xIcon = document.querySelector('.xIcon');
 
-ham[0].addEventListener("click", toggleMenu);
-ham[1].addEventListener("click", toggleMenu);
+ham[0].addEventListener('click', toggleMenu);
+ham[1].addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    xIcon.style.display = "none";
-    menuIcon.style.display = "block";
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    xIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
   } else {
-    menu.classList.add("showMenu");
-    menuIcon.style.display = "none";
-    xIcon.style.display = "block";
+    menu.classList.add('showMenu');
+    menuIcon.style.display = 'none';
+    xIcon.style.display = 'block';
   }
 }
